3c352dfba5ab20a4b5296516e925f692570cea70
DESARROLLO HAPPY INC <ti.ven97@hotmail.com>
2017-03-06 10:40:26 -0500

    webservice configurado para el servidor de happyinc

--- DatabaseConnection/mysql_login.php
+++ DatabaseConnection/mysql_login.php
@@ -3,7 +3,7 @@
  * Provee las constantes para conectarse a la base de datos
  * Mysql.
  */
-define("HOSTNAME", "happyhappy.com.co");// Nombre del host
+define("HOSTNAME", "happyhappyinc.com");// Nombre del host
 define("DATABASE", "control_access"); // Nombre de la base de datos
 define("USERNAME", "control_access"); // Nombre del usuario
 define("PASSWORD", "control_access2017"); // Nombre de la constraseña
